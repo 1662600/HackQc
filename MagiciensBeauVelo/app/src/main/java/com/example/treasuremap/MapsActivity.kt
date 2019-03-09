f66e7a7cdd8dbabc161dbd8c944e59650c64d23a
Samuel <samuel.parent13@gmail.com>
2019-03-08 23:31:43 -0500

    add adress converter to coordonates

--- MagiciensBeauVelo/app/src/main/java/com/example/treasuremap/MapsActivity.kt
+++ MagiciensBeauVelo/app/src/main/java/com/example/treasuremap/MapsActivity.kt
@@ -1,18 +1,36 @@
 package com.example.treasuremap
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import android.location.Geocoder
+
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
+
+    private lateinit var lastLocation: Location
+
+    private lateinit var map: GoogleMap
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +39,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
     }
 
     /**
@@ -33,11 +52,46 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        map = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val address = getLocationFromAddress(this, "1650, boulevard La Morille, Québec (QC) G2K 2L2");
+        map.addMarker(MarkerOptions().position(address!!).title("My address"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(address))
+        setUpMap()
+
     }
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+    }
+
+    fun getLocationFromAddress(context: Context, strAddress: String): LatLng? {
+        val coder = Geocoder(context)
+        val address: List<Address>?
+        var convertedAdress: LatLng? = null
+
+        try {
+            address = coder.getFromLocationName(strAddress, 5)
+            if (address == null) {
+                return null
+            }
+            val location = address[0]
+            location.getLatitude()
+            location.getLongitude()
+
+            convertedAdress = LatLng(location.getLatitude(), location.getLongitude())
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return convertedAdress
+
+    }
+
 }
