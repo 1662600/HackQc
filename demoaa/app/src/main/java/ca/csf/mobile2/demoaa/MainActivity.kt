bbf8456a6561a97e5909fffc1352df15a4077cbb
Sam <sberube1999@gmail.com>
2019-03-09 21:36:34 -0500

    Yeeeeee

--- demoaa/app/src/main/java/ca/csf/mobile2/demoaa/MainActivity.kt
+++ demoaa/app/src/main/java/ca/csf/mobile2/demoaa/MainActivity.kt
@@ -12,11 +12,16 @@ import org.androidannotations.annotations.*
 import java.io.InputStream
 import java.nio.charset.Charset
 import android.widget.Button
+import android.widget.TextView
+import com.example.demoaa.loisir.LOISIR_LIBRE
 import kotlinx.android.synthetic.main.categorie_activite_layout.view.*
+import ca.csf.mobile2.demoaa.Categorization.Categories
 
 
 @EActivity(R.layout.activity_main)
 class MainActivity : AppCompatActivity() {
+
+    var activitiesWithSelectedCategories : List<LOISIR_LIBRE> = listOf()
 
     var informationProvider = InformationProvider()
 
@@ -31,6 +36,9 @@ class MainActivity : AppCompatActivity() {
     @ViewById(R.id.backButton)
     protected lateinit var backButton : Button
 
+    @ViewById(R.id.indication)
+    protected lateinit var indication : TextView
+
     protected lateinit var loisirObject : Loisir
 
     @InstanceState
@@ -38,16 +46,21 @@ class MainActivity : AppCompatActivity() {
 
 
     @AfterViews
-    protected fun afterViews() {
+    protected fun afterViews()
+    {
 
         grid.adapter = adapter
         loisirObject = DeserialiseJSONFile()
 
-        grid.onItemClickListener = object : AdapterView.OnItemClickListener {
-            override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+        grid.onItemClickListener = object : AdapterView.OnItemClickListener
+        {
+            override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long)
+            {
                 // Get the GridView selected/clicked item text
-                selectedCategorieText = parent.getChildAt(position).categorieText.text.toString()
-
+                if(view!=null)
+                {
+                    selectedCategorieText = view.categorieText.text.toString()
+                }
                 Log.v("HAAAA", selectedCategorieText)
 
                 hideCategories()
@@ -56,18 +69,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    @Background
-    protected fun findSimilarWords() {
-
-    }
-
     @UiThread
-    protected fun showActivites() {
+    protected fun showActivites()
+    {
         backButton.visibility = View.VISIBLE
+        activitiesWithSelectedCategories = GetAllActivitiesWithSelectedCategorie()
+        indication.text = getString(R.string.activite)
     }
 
     @UiThread
-    protected fun hideActivites() {
+    protected fun hideActivites()
+    {
         backButton.visibility = View.INVISIBLE
     }
 
@@ -75,6 +87,7 @@ class MainActivity : AppCompatActivity() {
     protected fun showCategories()
     {
         grid.visibility = GridView.VISIBLE
+        indication.text = getString(R.string.categorie)
     }
 
 
@@ -104,5 +117,23 @@ class MainActivity : AppCompatActivity() {
         var content = ReadJSONFile()
 
         return gson.fromJson(content, Loisir::class.java)
+    }
+
+    private fun GetAllActivitiesWithSelectedCategorie() : List<LOISIR_LIBRE>
+    {
+        var subCategories: HashMap<String?, Categories> = informationProvider.GetSubCategoriesOf(Categories.valueOf(selectedCategorieText))
+
+        var activites : List<LOISIR_LIBRE> = loisirObject.LOISIRS_LIBRES.LOISIR_LIBRE
+
+        var activitiesFiltre : List<LOISIR_LIBRE> = listOf()
+
+        for(i in 0..subCategories.size -1)
+        {
+                activitiesFiltre += activites.filter { it.DESCRIPTION_NAT == subCategories.keys.elementAt(i) }
+        }
+
+        activitiesFiltre = activitiesFiltre.distinctBy {it.ADRESSE}
+
+        return activitiesFiltre
     }
 }
